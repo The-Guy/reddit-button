b73403879529a281ce56c23afce9b2da9e43469b
The-Guy <tjyocolano@gmail.com>
2015-05-05 14:40:00 -0400

    Original Upload

---
+++ data/panel.js
@@ -0,0 +1,8 @@
+$('#header').remove();
+$('.side').remove();
+$('.footer-parent').remove();
+$('#siteTable').remove();
+$('#progressIndicator').remove();
+$('.debuginfo').remove();
+$('section').remove();
+$('.content').css({"margin" : "auto"});
